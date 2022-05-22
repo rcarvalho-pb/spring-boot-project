40e6c1cc1d471d4cb9bc7b033124cc733229d880
Ramon Carvalho <rcarvalho.pb@gmail.com>
2022-05-22 11:54:11 -0300

    Creating instantianting operation

--- src/main/java/com/springcourse/cursomc/CursomcApplication.java
+++ src/main/java/com/springcourse/cursomc/CursomcApplication.java
@@ -1,13 +1,31 @@
 package com.springcourse.cursomc;
 
+import java.util.Arrays;
+
+import com.springcourse.cursomc.domain.Category;
+import com.springcourse.cursomc.repositories.CategoryRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
